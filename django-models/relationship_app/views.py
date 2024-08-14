9049b841531290b3e899b7e992ec0fd017582002
shoile azeez <shoabdulazeez@gmail.com>
2024-08-14 14:40:15 +0100

    Implementing Advanced Model Relationships in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book
+# from django.http import HttpResponse
 
 def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'book_list': books}  # Create a context dictionary with book list
-      return render(request, 'books/list_books.html', context)
+      return render(request, 'relationship_app/list_books.html', context)
       # return HttpResponse(book_list, content_type="text/plain")
   
 class BookDetailView(DetailView):
   """A class-based view for displaying details of a specific book."""
-  model = Book
-  template_name = 'books/book_detail.html'
   
-  def get_context_data(self, **kwargs):
+  def get_context_data(self, request):
     """Injects additional context data specific to the book."""
-    context = super().get_context_data(**kwargs)  # Get default context data
-    book = self.get_object()  # Retrieve the current book instance
-    context['average_rating'] = book.get_average_rating() 
+    # context = super().get_context_data(**kwargs)  # Get default context data
+    context = {'detail_view': book}
+    book = self.get_object()# Retrieve the current book instance
+    return render(request, 'relationship_app/library_detail.html', context)
+    # context['average_rating'] = book.get_average_rating() 
 # Create your views here.
