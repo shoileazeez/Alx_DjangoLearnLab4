eb4ef2f559e592b4c622c3e16cd74bf92c5632cb
shoile azeez <shoabdulazeez@gmail.com>
2024-08-14 14:10:35 +0100

    Implementing Advanced Model Relationships in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book
 
+def book_list(request):
+      """Retrieves all books and renders a template displaying the list."""
+      books = Book.objects.all()  # Fetch all book instances from the database
+      context = {'book_list': books}  # Create a context dictionary with book list
+      return render(request, 'books/book_list.html', context)
+  
+class BookDetailView(DetailView):
+  """A class-based view for displaying details of a specific book."""
+  model = Book
+  template_name = 'books/book_detail.html'
+  
+  def get_context_data(self, **kwargs):
+    """Injects additional context data specific to the book."""
+    context = super().get_context_data(**kwargs)  # Get default context data
+    book = self.get_object()  # Retrieve the current book instance
+    context['average_rating'] = book.get_average_rating() 
 # Create your views here.
