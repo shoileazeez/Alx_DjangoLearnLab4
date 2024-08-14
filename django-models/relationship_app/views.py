18a596b9b73c1dca9733b887af9216b087de1a7a
shoile azeez <shoabdulazeez@gmail.com>
2024-08-14 14:42:43 +0100

    Implementing Advanced Model Relationships in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book
+from .models import Library
 # from django.http import HttpResponse
 
 def book_list(request):
