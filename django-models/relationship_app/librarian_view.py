55fac70bd75942e662da7a3a957a22f9a736015e
shoile azeez <shoabdulazeez@gmail.com>
2024-08-17 20:03:48 +0100

    . Implementing User Authentication in Django

--- django-models/relationship_app/librarian_view.py
+++ django-models/relationship_app/librarian_view.py
@@ -1,10 +1,17 @@
-from django.contrib.auth.decorators import user_passes_test
+# from django.contrib.auth.decorators import user_passes_test
+# from django.shortcuts import render
+
+# def is_librarian(user):
+#     return user.userprofile.role == 'Librarian'
+
+
+# @user_passes_test(is_librarian)
+# def librarian_view(request):
+#     return render(request, 'templates/relationship_app/librarian_view.html')
+
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-
-@user_passes_test(is_librarian)
+@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
 def librarian_view(request):
-    return render(request, 'templates/relationship_app/librarian_view.html')
+    return render(request, 'librarian_view.html')
