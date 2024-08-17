ec4d74f296f88cc0c1f8d5ba3dee046feeecbaf2
shoile azeez <shoabdulazeez@gmail.com>
2024-08-17 18:45:40 +0100

    . Implementing User Authentication in Django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -41,7 +41,7 @@ def is_member(user):
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
+@user_passes_test(lambda u: u.userprofile.role == 'Librarians')
 def librarian_view(request):
     return render(request, 'templates/relationship_app/librarian_view.html')
 
