dfdbe3ef947b513429a21863557296f0bf20140b
shoile azeez <shoabdulazeez@gmail.com>
2024-08-17 20:29:32 +0100

    . Implementing User Authentication in Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -28,12 +28,13 @@
 
 # relationship_app/urls.py
 from django.urls import path
+from . import views
 from .views import admin_view, librarian_view,  member_view
 
 urlpatterns = [
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
 
 
