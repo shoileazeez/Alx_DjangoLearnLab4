87e4bc6dc0dd092ef605468eacf669287ca52524
shoile azeez <shoabdulazeez@gmail.com>
2024-08-19 07:12:38 +0100

    djnango view and url configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -48,10 +48,7 @@
 # def is_member(user):
 #     return user.userprofile.role == 'Member'
 
-# # Views
-# @user_passes_test(is_admin)
-# def admin_view(request):
-#     return render(request, 'relationship_app/admin_view.html')
+
 
 # @user_passes_test(is_librarian)
 # def librarian_view(request):
