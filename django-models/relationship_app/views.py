a1fb5625e42df2562f168cf6e1db7d5ca3d1a993
shoile azeez <shoabdulazeez@gmail.com>
2024-08-19 07:17:20 +0100

    djnango view and url configuration

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -235,3 +235,26 @@
 
 
 
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarians(user):
+    return user.userprofile.role == 'Librarians'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarians)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
