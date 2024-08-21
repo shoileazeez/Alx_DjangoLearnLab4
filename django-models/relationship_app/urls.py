5e438b4256acac49284354acca1e30a99f3f76a4
shoile azeez <shoabdulazeez@gmail.com>
2024-08-21 21:48:42 +0100

    error

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
 
     
     
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
 
