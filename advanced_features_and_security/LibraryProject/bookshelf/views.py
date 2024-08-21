19ef69f19aa10d93c19a86bb3b3e5a70564218ad
shoile azeez <shoabdulazeez@gmail.com>
2024-08-21 13:43:02 +0100

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,25 +3,27 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 
-@permission_required('your_app.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def view_article(request, pk):
     article = get_object_or_404(Article, pk=pk)
-    return render(request, 'your_app/view_article.html', {'article': article})
+    return render(request, 'bookshelf/view_article.html', {'article': article})
 
-@permission_required('your_app.can_create', raise_exception=True)
+@permission_required('bookshelf.can_create', raise_exception=True)
 def create_article(request):
     # Logic to create an article
-    return render(request, 'your_app/create_article.html')
+    return render(request, 'bookshelf/create_article.html')
 
-@permission_required('your_app.can_edit', raise_exception=True)
+@permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_article(request, pk):
     article = get_object_or_404(Article, pk=pk)
     # Logic to edit the article
-    return render(request, 'your_app/edit_article.html', {'article': article})
+    return render(request, 'bookshelf/edit_article.html', {'article': article})
 
-@permission_required('your_app.can_delete', raise_exception=True)
+@permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_article(request, pk):
     article = get_object_or_404(Article, pk=pk)
     # Logic to delete the article
-    return render(request, 'your_app/delete_article.html')
+    return render(request, 'bookshelf/delete_article.html')
+
+ book_list", "books
 # Create your views here.
