90f2c1d2a2c0320c996fadc18d4cbf5f9e99e773
shoile azeez <shoabdulazeez@gmail.com>
2024-08-21 11:52:40 +0100

    error

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -242,7 +242,7 @@ def is_admin(user):
     return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
-    return user.userprofile.role == 'librarians'
+    return user.userprofile.role == 'librarian'
 
 def is_member(user):
     return user.userprofile.role == 'Member'
@@ -265,7 +265,7 @@ from django.shortcuts import render
 
 
 def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == 'Librarian'
+    return user.is_authenticated and user.userprofile.role == 'librarian'
 
 
 @user_passes_test(is_librarian)
